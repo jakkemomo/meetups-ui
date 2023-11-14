a59433c74bccc58ea55c3cfeffdfa82c077121bf
Hsu-hsuuuu <and.natarov@yandex.ru>
2023-11-14 19:31:19 +0300

    feat: install react router dom

---
+++ src/Meetup.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Meetup.module.scss';
+
+function Meetup() {
+return (
+    <div>
+    </div>
+  );
+}
+
+export default Meetup;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Meetup from './Meetup';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Meetup />
   </React.StrictMode>
 );

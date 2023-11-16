834d8d8f29b90b9ea7eff7ef503c32241100433a
Hsu-hsuuuu <and.natarov@yandex.ru>
2023-11-16 23:18:42 +0300

    fix: import api key from .env file in component

--- src/pages/BackgroundMap/BackgroundMap.tsx
+++ src/pages/BackgroundMap/BackgroundMap.tsx
@@ -7,11 +7,15 @@ import {
     InfoWindow,
 } from '@vis.gl/react-google-maps';
 
+
+const apiKey: string = (process.env.REACT_APP_GOOGLE_MAP_API_KEY as string) 
+
+
 const BackgroundMap: FC = () => {
     const position = { lat: 53.90228, lng: 27.561831 };
 
 return (
-    <APIProvider apiKey='AIzaSyB1-1DC0D7WgsFMvTslrDNRmAI58irxToY'>
+    <APIProvider apiKey={apiKey}>
         <div style={{height: '100vh'}}>
             <Map zoom={12} center={position}></Map>
         </div>

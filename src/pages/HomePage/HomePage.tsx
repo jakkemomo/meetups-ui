2bf6423a37019f05055577a64159844c9a559b5e
Hsu-hsuuuu <and.natarov@yandex.ru>
2023-11-21 21:53:50 +0300

    feat: added execution of a request to get event data from the server

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,26 +1,49 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import {
     APIProvider,
     Map,
     AdvancedMarker,
     Pin,
     InfoWindow,
+    Marker,
 } from '@vis.gl/react-google-maps';
-
+import axios from 'axios';
 
 const apiKey: string = (process.env.REACT_APP_GOOGLE_MAP_API_KEY as string) 
 const mapId: string = (process.env.REACT_APP_GOOGLE_MAP_ID as string)
 
+
+interface Markers {
+    type: string
+}
+
 const HomePage: FC = () => {
     const position = { lat: 53.90228, lng: 27.561831 };
+    const [events, setEvents] = useState([])
+    
+    useEffect(() => {
+        axios.get('https://meetups-dev-6vuzexfx2q-lm.a.run.app/api/v1/markers/')
+        .then((response) => {
+            const markers = response.data.features;
+            setEvents(markers)
+            })
+        .catch((error) => {
+            console.error(error);
+            })
+    }, [])
+
+    console.log(events)
 
 return (
     <APIProvider apiKey={apiKey}>
         <div style={{height: '100vh'}}>
             <Map zoom={12} center={position} mapId={mapId}>
-                <AdvancedMarker position={position}>
-                    <Pin borderColor={'brown'}/>
-                </AdvancedMarker>
+                {events.map(event => 
+                    <AdvancedMarker position={position}>
+                        <Pin borderColor={'brown'}/>
+                    </AdvancedMarker> 
+                )}
+                
             </Map>
         </div>
     </APIProvider>

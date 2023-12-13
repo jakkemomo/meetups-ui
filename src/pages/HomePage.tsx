bd6f723f3242692d66dd9387f172f8e9901e899f
Hsu-hsuuuu <and.natarov@yandex.ru>
2023-12-13 20:33:44 +0300

    made a opening component infoWindow by clicking

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,11 @@
 import { FC } from 'react';
 import MapComponent from '../component/Map';
-import Calendar from '../component/Calendar';
+// import Calendar from '../component/Calendar';
 
 const HomePage: FC = () => {
     return (
         <div className={"h-screen"}>
-            <Calendar/>
+            {/* <Calendar/> */}
             < MapComponent />
         </div >
     );

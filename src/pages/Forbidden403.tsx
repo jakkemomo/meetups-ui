be327fd1de616970a730e2b577c40d59ae55b608
Tatiana Andreeva <snurnisyan@gmail.com>
2023-12-18 13:58:28 +0300

    fix: fix text in 403 page

--- src/pages/Forbidden403.tsx
+++ src/pages/Forbidden403.tsx
@@ -12,7 +12,7 @@ const Forbidden403: FC = () => {
     <section className='flex flex-col gap-20 m-auto'>
       <p className='text-9xl m-auto pb-20'>Error 403</p>
       <p className='text-4xl m-auto'>Доступ запрещен</p>
-      <p className='text-2xl m-auto'>У вас нет достаточно прав для доступа к этой странице</p>
+      <p className='text-2xl m-auto'>У вас недостаточно прав для доступа к этой странице</p>
       <button
         className='text-2xl bg-neutral-50 w-80 m-auto py-4 rounded-lg text-neutral-900 hover:opacity-80'
         onClick={onClick}

3d1fd49617158e86f88b523018ed00a8b3aebe92
Hsu-hsuuuu <and.natarov@yandex.ru>
2023-12-23 21:41:36 +0300

    added APIProvider with apiKey

--- src/component/Map.tsx
+++ src/component/Map.tsx
@@ -1,5 +1,6 @@
 import { FC, useEffect, useState } from 'react';
 import {
+    APIProvider,
     Map,
     AdvancedMarker,
     Pin,
@@ -7,8 +8,10 @@ import {
 } from '@vis.gl/react-google-maps';
 import { useGetMapQuery } from '../store/map/mapApi';
 import styles from './Map.module.scss';
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
 
-const mapId: string = (process.env.REACT_APP_GOOGLE_MAP_ID as string)
+const mapId: string = (process.env.REACT_APP_GOOGLE_MAP_ID as string);
+const apiKey: string = (process.env.REACT_APP_GOOGLE_MAP_API_KEY as string)
 
 const MapComponent: FC = () => {
     const position = { lat: 53.90228, lng: 27.561831 };
@@ -21,42 +24,43 @@ const MapComponent: FC = () => {
             setEvents(data.features)
     }, [data, isSuccess]);
 
-
-
+    // console.log(data.features.map( (el: any) => el.properties))
 
     const eventsArr = events.map(arr =>
         arr['geometry']['coordinates']).map(([lng, lat]) =>
             ({ lng, lat }));
 
     return (
-        <Map zoom={12} center={position} mapId={mapId} className={styles.map}>
-            {eventsArr.map((event, index) =>
-                <AdvancedMarker position={event} key={index} onClick={() => setOpen(index)}>
-                    {open === index && 
-                        <InfoWindow className={styles.info_window} position={event} >
-                            {data.features.map((el:any, i: number) => i === index ? 
-                                <div>
-                                    <p className={styles.info_window_p}>
-                                        {el.properties.name}
-                                    </p> 
-                                    <p className={styles.info_window_p}>
-                                        {el.properties.address}
-                                    </p> 
-                                    <p className={styles.info_window_p}>
-                                        {new Date(el.properties.start_date).toLocaleString()}
-                                    </p> 
-                                    <p className={styles.info_window_p}>
-                                        {new Date(el.properties.end_date).toLocaleString()}
-                                    </p> 
-                                    <button className={styles.info_window_button}>join</button>
-                                </div>
-                            : null)}
-                        </InfoWindow>}
-                    {/* {open === index && data.features.map((el:any) => <InfoWindow position={event} className="App"><p>{el.properties.name}</p></InfoWindow>)} */}
-                    <Pin borderColor={'brown'} />
-                </AdvancedMarker>
-            )}
-        </Map>
+        <APIProvider apiKey={apiKey}>
+            <Map zoom={12} center={position} mapId={mapId} className={styles.map}>
+                {eventsArr.map((event, index) =>
+                    <AdvancedMarker position={event} key={index} onClick={() => setOpen(index)}>
+                        {open === index && 
+                            <InfoWindow className={styles.info_window} position={event} >
+                                {data.features.map((el:any, i: number) => i === index ? 
+                                    <div>
+                                        <p className={styles.info_window_p}>
+                                            {el.properties.name}
+                                        </p> 
+                                        <p className={styles.info_window_p}>
+                                            {el.properties.address}
+                                        </p> 
+                                        <p className={styles.info_window_p}>
+                                            {new Date(el.properties.start_date).toLocaleString()}
+                                        </p> 
+                                        <p className={styles.info_window_p}>
+                                            {new Date(el.properties.end_date).toLocaleString()}
+                                        </p> 
+                                        <button className={styles.info_window_button}>join</button>
+                                    </div>
+                                : null)}
+                            </InfoWindow>}
+                        {/* {open === index && data.features.map((el:any) => <InfoWindow position={event} className="App"><p>{el.properties.name}</p></InfoWindow>)} */}
+                        <Pin borderColor={'brown'} />
+                    </AdvancedMarker>
+                )}
+            </Map>
+        </APIProvider>
     );
 }
 

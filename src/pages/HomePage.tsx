332f9d2e0778a6aca676c2daed9d456d425d74b7
Alexander Romanovich <alexolikov@gmail.com>
2023-12-29 16:04:43 +0300

    Fixed header

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,14 @@
 import {FC} from 'react';
 import MapComponent from '../component/Map';
 import EventsSection from "../component/Events/EventsSection";
+import Header from '../component/Header/Header';
 // import Calendar from '../component/Calendar';
 
 const HomePage: FC = () => {
     return (
         <div className={"h-screen w-screen"}>
-          {/* <Calendar/> */}
+           {/*<Calendar/>*/}
+          <Header/>
           <MapComponent />
           <EventsSection />
         </div >

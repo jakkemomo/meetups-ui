9936ad401a968fb266bdc306e9edf7680bc88b36
Alexander Romanovich <alexolikov@gmail.com>
2023-12-30 09:52:02 +0300

    Extended black bg

--- src/component/Events/EventsSection.tsx
+++ src/component/Events/EventsSection.tsx
@@ -6,7 +6,7 @@ import {ReactElement} from "react";
 function EventsSection(): ReactElement {
 
   return (
-    <section className='flex flex-col pl-20 pt-8 pb-4 pr-5 gap-14 absolute bottom-0 left-0 bg-gradient-to-r from-neutral-950 from-80% w-860'>
+    <section className='flex flex-col pl-20 pt-80 pb-4 pr-5 gap-14 absolute bottom-0 left-0 bg-gradient-to-r from-neutral-950 from-80% w-860'>
       <SearchMenu />
       <EventList events={mockEventList}/>
     </section>

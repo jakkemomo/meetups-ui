042f8ae287d52a7ffe8ed29f54fed37db7175138
Alexander Romanovich <alexolikov@gmail.com>
2024-02-04 17:31:03 +0200

    Removed links and moved redirect logic to button onclick func

--- src/features/authentication/logout/ui/LogoutButton.tsx
+++ src/features/authentication/logout/ui/LogoutButton.tsx
@@ -1,22 +1,25 @@
-import {Link} from "react-router-dom";
 import {RefreshToken} from "@/shared/model/types.ts";
 import {useLogoutMutation} from "@/entities/session/api/sessionApi.ts";
 import {Button} from "@/shared";
 import React from "react";
+import {useNavigate} from "react-router-dom";
 
 
 export function LogoutButton(token: RefreshToken) {
-  const [logoutTrigger] = useLogoutMutation()
-  const onConfirmLogout = (e: React.MouseEvent<HTMLElement>) => {
-    e.stopPropagation()
-    e.preventDefault()
-    logoutTrigger(token)
-        .unwrap()
-        .catch((error) => {throw new Error(error.data.details)})
-  }
+    const [logoutTrigger] = useLogoutMutation()
+    const navigate = useNavigate();
+
+    const onConfirmLogout = (e: React.MouseEvent<HTMLElement>) => {
+        e.stopPropagation()
+        e.preventDefault()
+        logoutTrigger(token)
+            .unwrap()
+            .then(() => navigate('/'))
+            .catch((error) => {
+                throw new Error(error.data.details)
+            })
+    }
   return (
-      <Link to="/" onClick={onConfirmLogout}>
-        <Button HTMLType='button' type='secondary'>Выйти</Button>
-      </Link>
+      <Button HTMLType='button' type='secondary' onClick={onConfirmLogout}>Выйти</Button>
   )
 }

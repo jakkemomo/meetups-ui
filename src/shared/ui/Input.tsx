74b39c5ca23e88a069e9e71f64dbd5b3e2f3117d
Ivan <ButorinLP@yandex.ru>
2024-02-14 20:50:46 +0300

    fix: add void tag in Input

--- src/shared/ui/Input.tsx
+++ src/shared/ui/Input.tsx
@@ -30,30 +30,32 @@ export function Input({ onChange, HTMLType, iconType, placeholder, extraBoxClass
   };
 
   return (
-    <div className={`bg-custom-gray rounded-[10px] border w-[316px] md:w-80 ${extraBoxClass} ${error ? 'border-input-error' : 'border-transparent'}`}>
-      <div className={`flex items-center h-48px w-full overflow-hidden p-3.5 ${extraInputClass}`}>
-        {iconType &&
-          <div
-            className='min-w-6 h-6 bg-center bg-no-repeat bg-cover'
-            style={{ backgroundImage: `url("/images/${iconType}.svg")` }}
-          />}
-        <input
-          {...hookFormValues}
-          onChange={onChange}
-          value={inputValue}
-          type={type}
-          defaultValue={defaultValue}
-          placeholder={placeholder}
-          aria-invalid={error ? 'true' : 'false'}
-          className={`h-full w-full outline-none text-black bg-inherit px-3 font-normal text-base md:text-lg`}
-        />
-        {(HTMLType === 'password') &&
-          <div
-            className='min-w-6 h-6 bg-center bg-no-repeat cursor-pointer'
-            style={{ backgroundImage: `url("/images/${passwordIcon}.svg")` }}
-            onClick={toggleShowPassword}
-          />}
+    <>
+      <div className={`bg-custom-gray rounded-[10px] border w-[316px] md:w-80 ${extraBoxClass} ${error ? 'border-input-error' : 'border-transparent'}`}>
+        <div className={`flex items-center h-48px w-full overflow-hidden p-3.5 ${extraInputClass}`}>
+          {iconType &&
+            <div
+              className='min-w-6 h-6 bg-center bg-no-repeat bg-cover'
+              style={{ backgroundImage: `url("/images/${iconType}.svg")` }}
+            />}
+          <input
+            {...hookFormValues}
+            onChange={onChange}
+            value={inputValue}
+            type={type}
+            defaultValue={defaultValue}
+            placeholder={placeholder}
+            aria-invalid={error ? 'true' : 'false'}
+            className={`h-full w-full outline-none text-black bg-inherit px-3 font-normal text-base md:text-lg`}
+          />
+          {(HTMLType === 'password') &&
+            <div
+              className='min-w-6 h-6 bg-center bg-no-repeat cursor-pointer'
+              style={{ backgroundImage: `url("/images/${passwordIcon}.svg")` }}
+              onClick={toggleShowPassword}
+            />}
+        </div>
       </div>
-    </div>
+    </>
   )
 }

db5e3596f3c5041b9f7048ec1efd1399917e502c
Ivan <ButorinLP@yandex.ru>
2024-02-15 14:46:04 +0300

    fix: design fixes

--- src/widgets/Header/ui/Logo.tsx
+++ src/widgets/Header/ui/Logo.tsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 export default function Logo(): ReactElement {
   return (
     <Link to={'/'}>
-      <div className="bg-logo w-[165px] h-[60px]"></div>
+      <div className="bg-logo w-[165px] h-[50px]"></div>
     </Link>
   );
 }

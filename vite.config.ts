95c2b195a5e589ff5190814cd0fe74da463ad4fe
Alexander Romanovich <alexolikov@gmail.com>
2024-03-03 08:10:08 +0200

    debug: added system env to be used as well as .env env

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,34 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
-import { version } from './package.json'
 import { resolve } from 'path'
 
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: { 'process.env': {} },
-  resolve: {
-    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    watch: {
-      usePolling: true
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      'process.env.VITE_BASE_URL_API': JSON.stringify(env.BASE_URL_API),
+    },
+    resolve: {
+      alias: [{find: "@", replacement: resolve(__dirname, "./src")}],
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 3000,
+      watch: {
+        usePolling: true
+      }
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: `[name].js`,
+          chunkFileNames: `[name].js`,
+          assetFileNames: `[name].[ext]`
+        }
       }
     }
-  },
+  }
 })

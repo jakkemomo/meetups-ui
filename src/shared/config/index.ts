191a769b185ca3984ea127edfb3e13d8bba15196
Alexander Romanovich <alexolikov@gmail.com>
2024-03-03 08:36:37 +0200

    debug: added more system envs

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -1,5 +1,5 @@
 export const config = {
-  GOOGLE_MAP_API_KEY: import.meta.env.VITE_APP_GOOGLE_MAP_API_KEY,
-  GOOGLE_MAP_ID: import.meta.env.VITE_APP_GOOGLE_MAP_ID,
-  BASE_URL_API: import.meta.env.VITE_BASE_URL_API || process.env.VITE_BASE_URL_API
+  GOOGLE_MAP_API_KEY: process.env.VITE_APP_GOOGLE_MAP_API_KEY || import.meta.env.VITE_APP_GOOGLE_MAP_API_KEY,
+  GOOGLE_MAP_ID: process.env.VITE_APP_GOOGLE_MAP_ID || import.meta.env.VITE_APP_GOOGLE_MAP_ID,
+  BASE_URL_API: process.env.VITE_BASE_URL_API || import.meta.env.VITE_BASE_URL_API
 } as const
